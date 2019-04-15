78b956d191f56e0d239619140a7a75d038b791fb
Silu Hu <erinh816@gmail.com>
2019-04-14 20:04:21 -0400

    Adding first component

--- max-burger/src/App.js
+++ max-burger/src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import IcanbeAnything from './Person/Person';
+// the Person after import can be anything
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> Hi This is Another Burger App </h1>
+        <p> This is really working </p>
+        <IcanbeAnything />
+        <IcanbeAnything />
+        <IcanbeAnything />
       </div>
     );
   }
